401f8c14105ff43d1339d2f6c62987ff1dc39f01
Ali Norouziani <norouziani.ali@gmail.com>
2024-12-15 22:53:40 +0330

    Create Training.py

---
+++ Training.py
@@ -0,0 +1,109 @@
+"""
+Contains functions for training and tested a PyTorch model.
+"""
+import torch
+from tqdm.auto import tqdm
+from typing import Dict, List, Tuple
+
+def train(model: torch.nn.Module,
+          dataloader: torch.utils.data.DataLoader,
+          loss_fn: torch.nn.Module,
+          optimizer: torch.optim.Optimizer,
+          device: torch.device) -> Tuple[float, float]:
+    """Trains a PyTorch model for a single epoch.
+    Args:
+    model: A PyTorch model to be trained.
+    dataloader: A DataLoader instance for the model to be trained on.
+    loss_fn: A PyTorch loss function to minimize.
+    optimizer: A PyTorch optimizer to help minimize the loss function.
+    device: A target device to compute on (e.g. "cuda" or "cpu").
+    Returns:
+    A tuple of training loss and training accuracy metrics.
+    In the form (train_loss, train_accuracy).
+    """
+    model.train()
+    train_loss, train_acc = 0, 0
+    for batch, (X, y) in enumerate(dataloader):
+      X, y = X.to(device), y.to(device)
+      y_pred = model(X)
+      loss = loss_fn(y_pred, y)
+      train_loss += loss.item()
+      optimzer.zero_grad()
+      loss.backward()
+      optimizer.step()
+      y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+      train_acc += (y_pred_class == y).sum().item()/len(y_pred)
+    
+    train_loss /= len(dataloader)
+    train_acc /= len(dataloader)
+    return train_loss, train_acc
+
+def test_step(model: torch.nn.Module,
+              dataloader: torch.utils.data.DataLoader,
+              loss_fn: torch.nn.Module,
+              device: torch.device) -> Tuple[float, float]:
+    """ Tests a PyTorch model for a single epoch.
+     Args:
+    model: A PyTorch model to be tested.
+    dataloader: A DataLoader instance for the model to be tested on.
+    loss_fn: A PyTorch loss function to calculate loss on the test data.
+    device: A target device to compute on (e.g. "cuda" or "cpu").
+
+    Returns:
+    A tuple of testing loss and testing accuracy metrics.
+    In the form (test_loss, test_accuracy).
+    """
+    model.eval()
+    test_loss, test_acc = 0, 0
+    for batch, (X, y) in enumerate(dataloader):
+      X, y = X.to(device), y.to(device)
+      test_pred_logits = model(x)
+      loss = loss_fn(test_pred_logits, y)
+      test_lost += loss.item()
+      test_pred_labels = test_pred_logits.argmax(dim=1)
+      test_acc += (test_pred_labels == y).sum().item()/len(test_pred_labels)
+    test_loss = test_loss / len(dataloader)
+    test_acc = test_acc / len(dataloader)
+    return test_loss, test_acc
+
+def train(model: torch.nn.Module,
+          train_dataloader: torch.utils.data.DataLoader,
+          test_dataloader: torch.utils.data.DataLoader,
+          optimizer: torch.optim.Optimizer,
+          loss_fn: torch.nn.Module,
+          epochs: int,
+          device: torch.device) -> Dict[str, List]:
+    """Trains and tests a Pytorch model with the help of train_step() and test_step() functions.
+     Args:
+    model: A PyTorch model to be trained and tested.
+    train_dataloader: A DataLoader instance for the model to be trained on.
+    test_dataloader: A DataLoader instance for the model to be tested on.
+    optimizer: A PyTorch optimizer to help minimize the loss function.
+    loss_fn: A PyTorch loss function to calculate loss on both datasets.
+    epochs: An integer indicating how many epochs to train for.
+    device: A target device to compute on (e.g. "cuda" or "cpu").
+
+    Returns:
+    A dictionary of training and testing loss as well as training and
+    testing accuracy metrics. Each metric has a value in a list for 
+    each epoch.
+    """
+    results = {"train_loss": [],
+               "train_acc": [],
+               "test_loss": [],
+               "test_acc": []}
+    model.to(device)
+    for epoch in tqdm(range(epochs)):
+      train_loss, train_acc = train_step(model, train_dataloader, loss_fn, optimizer, device)
+      test_loss, test_acc = test_step(model, test_dataloader, loss_fn, device)
+    print(f"Epoch: {epoch+1} | "
+          f"train_loss: {train_loss:.2f} | "
+          f"train_acc: {train_acc:.2f} | "
+          f"test_loss: {test_loss:.2f} | "
+          f"test_acc: {test_acc:.2f}")
+    results["train_loss"].append(train_loss)
+    results["train_acc"].append(train_acc)
+    results["test_loss"].append(test_loss)
+    results["test_acc"].append(test_acc)
+
+    return results
