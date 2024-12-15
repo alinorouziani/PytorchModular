f83c3d11087986fac8dedd875d78464f05657485
Ali Norouziani <norouziani.ali@gmail.com>
2024-12-15 23:18:44 +0330

    Update SetupData.py

--- SetupData.py
+++ SetupData.py
@@ -10,7 +10,7 @@ import os
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-def create_dataloaders(train_dir: str, test_dir: str, transform: transforms.Compose, batch_size:int, num_workers: int):
+def create_dataloaders(train_dir: str, test_dir: str, transform: transforms.Compose, batch_size:int):
   """
   Creates training and testing Dataloader.
   Args:
@@ -22,7 +22,7 @@ def create_dataloaders(train_dir: str, test_dir: str, transform: transforms.Comp
   A tuple of (train_dataloader, test_dataloader, class_names).
   """
   train_data = datasets.ImageFolder(train_dir, transform = transform)
-  test_data = datasets.ImageFolder(test_dir, tranform = transform)
+  test_data = datasets.ImageFolder(test_dir, transform = transform)
   
   class_names = train_data.classes
 
